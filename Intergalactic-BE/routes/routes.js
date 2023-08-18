04ca4e0722946023873c1dfd6d2f525bcebe9773
chamikaEkanayaka <chamikaekanayaka@icloud.com>
2023-08-18 09:04:46 +0530

    18AUG2023

--- Intergalactic-BE/routes/routes.js
+++ Intergalactic-BE/routes/routes.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const test = require('./../functions/test/test');
+const booking = require('./../functions/booking/booking');
 
 //__________Server test__________
 router.get('/test', (req, res) => {
     test(req, res);
 });
+
+router.get('/test-booking', (req, res)=>{
+    booking(req, res);
+})
+
 
 module.exports = router;
